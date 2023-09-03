33df8ef1cb3b03af61fa45b237bd6059c7a5e83c
Naina Gupta <naina24gupta@gmail.com>
2023-09-03 13:45:24 +0530

    Updates

---
+++ precache-manifest.8a9ec2bfb7b81355001bd671785223e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "767440c901403b5005bbb061e0a8447f",
+    "url": "/Pizza-order-app/index.html"
+  },
+  {
+    "revision": "95e8d7c3e0a1fdc6f6a7",
+    "url": "/Pizza-order-app/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "7d9592e3132f79db47b9",
+    "url": "/Pizza-order-app/static/css/main.46bb3959.chunk.css"
+  },
+  {
+    "revision": "95e8d7c3e0a1fdc6f6a7",
+    "url": "/Pizza-order-app/static/js/2.f2fed39e.chunk.js"
+  },
+  {
+    "revision": "82e8035e49740f791fa94175bc04ac4e",
+    "url": "/Pizza-order-app/static/js/2.f2fed39e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d9592e3132f79db47b9",
+    "url": "/Pizza-order-app/static/js/main.67e0eea1.chunk.js"
+  },
+  {
+    "revision": "b876c979756dd91aef9e",
+    "url": "/Pizza-order-app/static/js/runtime-main.8673476d.js"
+  }
+]);
